ae94c7738b349cb80650c98b5d7b391f285b80a9
Siarhei Petrachenka <s.petrachenka@godeltech.com>
2021-08-12 17:07:03 +0300

    Add unit tests for creating employee

---
+++ tests/PayrollPreparation.UnitTests/AddEmployeeUnitTests/AddSalariedEmployeeUnitTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using Moq;
+using Moq.AutoMock;
+using PayrollPreparation.Application.AddEmployee.CommandHandlers;
+using PayrollPreparation.Application.AddEmployee.Commands;
+using PayrollPreparation.Application.Common.Contracts;
+using PayrollPreparation.Domain;
+using Xunit;
+
+namespace PayrollPreparation.UnitTests.AddEmployeeUnitTests
+{
+    public class AddSalariedEmployeeUnitTests
+    {
+        private readonly Fixture _fixture;
+        private readonly AutoMocker _autoMocker;
+
+        private readonly Mock<IPayrollDatasource> _mockPayrollDatasource;
+
+        public AddSalariedEmployeeUnitTests()
+        {
+            _fixture = new Fixture();
+            _autoMocker = new AutoMocker();
+
+            _mockPayrollDatasource = _autoMocker.GetMock<IPayrollDatasource>();
+        }
+
+        [Fact]
+        public async Task Handle_CreateHourlyEmployee()
+        {
+            //Arrange
+            _mockPayrollDatasource
+                .Setup(_ => _.AddEmployee(It.IsNotNull<Employee>()))
+                .Returns(() => _fixture.Create<Guid>());
+
+            var command = _fixture.Create<AddSalariedEmployeeCommand>();
+            var commandHandler = _autoMocker.CreateInstance<AddSalariedEmployeeCommandHandler>();
+
+            //Act
+            await commandHandler.Handle(command, CancellationToken.None);
+
+            //Assert
+            _mockPayrollDatasource.Verify(_ => _.AddEmployee(It.IsNotNull<Employee>()), Times.Once);
+        }
+    }
+}
